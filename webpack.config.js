aba7effea0b8091a073cb635bd1bdb58bfcdc2bc
cycgit <cyckoo@qq.com>
2018-10-01 00:46:36 +0800

    fix: umd

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+    ],
+  },
+  plugins: [],
+  externals: {
+    react: 'React',
+    'react-dom': 'ReactDOM',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  output: {
+    filename: 'umd.js',
+    path: path.resolve(__dirname, 'lib'),
+    libraryTarget: 'umd',
+  },
+  mode: 'production',
+};
